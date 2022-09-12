af8956430304b76886dc57cbb526943d64fa6824
alvezandreluiz <alvezandreluiz@gmail.com>
2022-09-12 18:35:58 -0300

    first commit

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Cliente from "../core/Cliente";
+import Botao from "./Botao";
+import Entrada from "./Entrada";
+
+interface FormularioProps {
+    cliente: Cliente
+    clienteMudou?: (cliente: Cliente) => void
+    cancelado?: () => void
+}
+
+export default function Formulario(props: FormularioProps) {
+    const id = props.cliente?.id
+    const [nome, setNome] = useState(props.cliente?.nome ?? '')
+    const [idade, setIdade] = useState(props.cliente?.idade ?? 0)
+    return (
+        <div>
+            {id ? (
+                <Entrada
+                    somenteLeitura
+                    texto="Code"
+                    valor={id}
+                    className="mb-5"
+                />
+            ) : false}
+            <Entrada
+                texto="Name"
+                valor={nome}
+                valorMudou={setNome}
+                className="mb-5"
+            />
+            <Entrada
+                texto="Age"
+                tipo="number"
+                valor={idade}
+                valorMudou={setIdade}
+            />
+            <div className="flex justify-end mt-7">
+                <Botao cor="blue" className="mr-2"
+                    onClick={() => props.clienteMudou?.(new Cliente(nome, +idade, id))}>
+                    {id ? 'Change' : 'Save'}
+                </Botao>
+                <Botao onClick={props.cancelado}>
+                    Cancel
+                </Botao>
+            </div>
+        </div>
+    )
+}
